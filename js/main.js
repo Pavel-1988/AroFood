270f6bb0237ad45d215605ea58231d68843aa5ea
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2023-01-08 13:33:21 +0200

    13cmt

--- js/main.js
+++ js/main.js
@@ -71,3 +71,27 @@ function scrollTop() {
     }
 }
 window.addEventListener('scroll', scrollTop)
+
+//Dark Light Theme
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => {
+    document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+}
+const getCurrentIcon = () => {
+    themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+}
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+})
